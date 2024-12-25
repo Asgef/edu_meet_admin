438fe4248321250d0d77e4cf1060a796edf485db
Asgef <asgefes1@gmail.com>
2024-12-25 15:00:16 +0300

    Fix import error in admin_panel/__init__.py

--- edu_meet_admin_panel/admin_panel/__init__.py
+++ edu_meet_admin_panel/admin_panel/__init__.py
@@ -1,7 +1,13 @@
-from edu_meet_admin_panel.admin_panel.slot_admin import SlotAdmin
-from edu_meet_admin_panel.admin_panel.order_admin import OrderAdmin
-from edu_meet_admin_panel.admin_panel.user_admin import UserAdmin
-from edu_meet_admin_panel.admin_panel.subject_admin import AcademicSubjectAdmin
+try:
+    from edu_meet_admin_panel.admin_panel.slot_admin import SlotAdmin
+    from edu_meet_admin_panel.admin_panel.order_admin import OrderAdmin
+    from edu_meet_admin_panel.admin_panel.user_admin import UserAdmin
+    from edu_meet_admin_panel.admin_panel.subject_admin import AcademicSubjectAdmin
+except ImportError:
+    SlotAdmin = None
+    OrderAdmin = None
+    UserAdmin = None
+    AcademicSubjectAdmin = None
 
 __all__ = [
     "SlotAdmin",
